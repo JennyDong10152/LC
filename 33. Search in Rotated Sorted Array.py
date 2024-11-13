3e12136541d5a470cae4549bf9eddec85e223b16
JennyDong1015 <zd66@cornell.edu>
2024-11-13 13:59:02 +0800

    11/11

---
+++ 33. Search in Rotated Sorted Array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            midV = nums[mid]
+
+            if midV == target:
+                return mid
+
+            if midV >= nums[left]:
+                if nums[left] <= target and target <= nums[mid]:
+                    right = mid-1
+                else:
+                    left = mid+1
+            else:
+                if nums[mid] <= target and target <= nums[right]:
+                    left = mid+1
+                else:
+                    right = mid-1
+        return -1
+
+        
