04877df45c52143f53ea424ad0767958fce347ff
JennyDong1015 <zd66@cornell.edu>
2024-11-14 19:45:34 +0800

    review

---  436. Find Right Interval.py
+++  436. Find Right Interval.py
@@ -5,19 +5,20 @@ class Solution:
             interval.append([i,j,idx])
         interval.sort()
 
-        starts = [i for [i,_,_] in interval]
+        starts = [i for i,_,_ in interval]
         ans = [-1] * len(starts)
 
         for i, j, idx_o in interval:
             left = 0
-            right = len(starts)
+            right = len(interval)
             while left < right:
                 mid = left + (right-left)//2
                 midV = starts[mid]
+
                 if midV >= j:
                     right = mid
                 else:
-                    left = mid + 1
+                    left = mid+1
             if left < len(starts):
                 ans[idx_o] = interval[left][2]
-        return ans 
+        return ans
