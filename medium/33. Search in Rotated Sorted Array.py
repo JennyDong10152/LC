b8b475ab0b551e9a39a85921ba17d9cf28c6d914
JennyDong1015 <zd66@cornell.edu>
2024-11-18 00:05:59 +0800

    review 11/17

--- medium/33. Search in Rotated Sorted Array.py
+++ medium/33. Search in Rotated Sorted Array.py
@@ -9,14 +9,14 @@ class Solution:
 
             if midV == target:
                 return mid
-            elif midV >= nums[left]:
+            if midV >= nums[left]:
                 if nums[left] <= target <= midV:
-                    right = mid - 1
+                    right = mid-1
                 else:
                     left = mid + 1
             else:
                 if midV <= target <= nums[right]:
                     left = mid + 1
                 else:
-                    right = mid - 1
+                    right = mid -1
         return -1
