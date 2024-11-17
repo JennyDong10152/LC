87b3440c0620c257cb184920efa9839df2a3652b
JennyDong1015 <zd66@cornell.edu>
2024-11-17 23:57:44 +0800

    review 11/17

--- medium/875. Koko Eating Bananas.py
+++ medium/875. Koko Eating Bananas.py
@@ -2,24 +2,20 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         left = 1
         right = max(piles)
-
+        
         while left < right:
             mid = left + (right-left)//2
-            hours = self.count(piles, mid) #num hours needed
+            hours = self.count(piles, mid)
             if hours > h:
                 left = mid + 1
             else:
                 right = mid
         return left
     
-    
-    def count(self, piles, mid):
+    def count(self, piles, target):
         cnt = 0
-        
         for p in piles:
-            if p%target != 0:
+            if p % target:
                 cnt += 1
-            cnt += p//target
+            cnt += p // target
         return cnt
-
-        
