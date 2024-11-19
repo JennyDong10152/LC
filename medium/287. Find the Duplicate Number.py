a16e2500a812f4aa8e9551d0cd8532bdb87c09b5
JennyDong1015 <zd66@cornell.edu>
2024-11-19 23:43:05 +0700

    update

--- medium/287. Find the Duplicate Number.py
+++ medium/287. Find the Duplicate Number.py
@@ -1,19 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         left = 1
-        right = len(nums)-1
-        dup = 0
+        right = len(nums)
 
-        while left <= right:
+        while left < right:
             mid = left + (right-left)//2
-
             cnt = 0
             for i in nums:
                 if i <= mid:
                     cnt += 1
-            if cnt <= mid:
-                left = mid + 1
+            if cnt > mid:
+                right = mid
             else:
-                dup = mid
-                right = mid - 1
-        return dup
+                left = mid + 1
+        return right
