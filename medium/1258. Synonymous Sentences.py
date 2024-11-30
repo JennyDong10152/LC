c2c1670a0550c9da0a86ba4a85564b3be56d37f2
JennyDong1015 <zd66@cornell.edu>
2024-11-30 23:25:25 +0800

    new

---
+++ medium/1258. Synonymous Sentences.py
@@ -0,0 +1,48 @@
+class Solution:
+    def generateSentences(self, synonyms: List[List[str]], text: str) -> List[str]:
+        parent = {}
+        
+        for x, y in synonyms:
+            if x not in parent:
+                parent[x] = x
+            if y not in parent:
+                parent[y] = y
+            self.union(parent, x, y)
+        
+        syn_map = defaultdict(list)
+        for key in parent:
+            root = self.find(parent, key)
+            syn_map[root].append(key)
+        for key in syn_map:
+            syn_map[key] = sorted(syn_map[key])
+        
+        words = text.split()
+        n = len(words)
+        ans = []
+        self.makeSentence(parent, syn_map, words, 0, [], n, ans)
+        
+        return sorted(ans)
+
+    def find(self, parent, x):
+        if parent[x] != x:
+            parent[x] = self.find(parent, parent[x])
+        return parent[x]
+    
+    def union(self, parent, x, y):
+        root_x = self.find(parent, x)
+        root_y = self.find(parent, y)
+        if root_x != root_y:
+            parent[root_x] = root_y
+
+    def makeSentence(self, parent, syn_map, words, idx, curr_sentence, n, ans):
+        if idx == n:
+            ans.append(" ".join(curr_sentence))
+            return
+        
+        word = words[idx]
+        if word in parent:
+            root_word = self.find(parent, word)
+            for synonym in syn_map[root_word]:
+                self.makeSentence(parent, syn_map, words, idx + 1, curr_sentence + [synonym], n, ans)
+        else:
+            self.makeSentence(parent, syn_map, words, idx + 1, curr_sentence + [word], n, ans)
