d68a1699f9583c83e4ba09cdc100c2798acbd41f
JennyDong1015 <zd66@cornell.edu>
2024-12-01 15:14:17 +0800

    review

--- hard/1591. Strange Printer II.py
+++ hard/1591. Strange Printer II.py
@@ -1,39 +1,40 @@
 class Solution:
     def isPrintable(self, targetGrid: List[List[int]]) -> bool:
+        parent = {}
+        bounds = defaultdict(lambda: [m, -1, n, -1])
         m = len(targetGrid)
         n = len(targetGrid[0])
-        bounds = defaultdict(lambda: [n, -1, m, -1])
+        graph = defaultdict(list)
         colors = set()
-        graph = defaultdict(set)
         degree = defaultdict(int)
 
         for i in range(m):
             for j in range(n):
                 color = targetGrid[i][j]
                 colors.add(color)
-                left, right, top, bot = bounds[color]
-                bounds[color] = min(left, j), max(right, j), min(top, i), max(bot, i)
+                top, bot, left, right = bounds[color]
+                bounds[color] = [min(top, i), max(bot, i), min(left, j), max(right, j)]
         
         for color in colors:
-            left, right, top, bot = bounds[color]
+            top, bot, left, right = bounds[color]
             for i in range(top, bot+1):
                 for j in range(left, right+1):
-                    if targetGrid[i][j] != color:
-                        if color not in graph[targetGrid[i][j]]:
-                            graph[targetGrid[i][j]].add(color)
+                    if color != targetGrid[i][j]:
+                        if not color in graph[targetGrid[i][j]]:
+                            graph[targetGrid[i][j]].append(color)
                             degree[color] += 1
+        
         q = deque()
         for color in colors:
             if not degree[color]:
                 q.append(color)
-        
+
         order = []
         while q:
             curr = q.popleft()
-            order.append(q)
+            order.append(curr)
             for neighbor in graph[curr]:
                 degree[neighbor] -= 1
                 if not degree[neighbor]:
                     q.append(neighbor)
-                    
         return len(order) == len(colors)
--- hard/2392. Build a Matrix With Conditions.py
+++ hard/2392. Build a Matrix With Conditions.py
@@ -3,34 +3,39 @@ class Solution:
         self.k = k
         row_order = self.sort(rowConditions)
         col_order = self.sort(colConditions)
-        col_order = {x : i for i, x in enumerate(col_order)}
-        grid = [[0 for i in range(k)] for i in range(k)]
 
+        if not row_order or not col_order:
+            return []
+
+        col_order = {x : i for i, x in enumerate(col_order)}
+        grid = [[0 for _ in range(k)] for _ in range(k)]
+        
         for i, x in enumerate(row_order):
             if not x in col_order:
                 break
             j = col_order[x]
             grid[i][j] = x
         
-        for i in grid:
-            if not any(i):
+        for row in grid:
+            if not any(row):
                 return []
         return grid
     
     def sort(self, relations):
         graph = defaultdict(list)
-        degree = [0] * (self.k + 1)
+        degree = [0] * (self.k+1)
 
-        for prev, num in relations:
-            graph[prev].append(num)
-            degree[num] += 1
+        for prev, x in relations:
+            if not x in graph[prev]:
+                graph[prev].append(x)
+                degree[x] += 1
         
         q = deque()
-        order = []
-
         for i in range(1, self.k+1):
             if not degree[i]:
                 q.append(i)
+
+        order = []
         while q:
             curr = q.popleft()
             order.append(curr)
@@ -38,5 +43,4 @@ class Solution:
                 degree[neighbor] -= 1
                 if not degree[neighbor]:
                     q.append(neighbor)
-
         return order if not any(degree) else []
