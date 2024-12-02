7133a1bfcf416d02c7f5cb44dbc1f6ad55915d28
JennyDong1015 <zd66@cornell.edu>
2024-12-03 00:30:24 +0800

    new

---
+++ medium/1061. Lexicographically Smallest Equivalent String.py
@@ -0,0 +1,26 @@
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        parent = {c:c for c in s1}
+        parent.update({c: c for c in s2})
+
+        for c, d in zip(s1, s2):
+            self.union(parent, c, d)
+        ans = ""
+        for c in baseStr:
+            ans += self.find(parent, c)
+        return ans
+    
+    def union(self, parent, x, y):
+        root_x = self.find(parent, x)
+        root_y = self.find(parent, y)
+        if root_x < root_y:
+            parent[root_y] = root_x
+        else:
+            parent[root_x] = root_y
+    
+    def find(self, parent, x):
+        if x not in parent:
+            return x
+        if parent[x] != x:
+            parent[x] = self.find(parent, parent[x])
+        return parent[x]
---
+++ medium/947. Most Stones Removed with Same Row or Column.py
@@ -0,0 +1,37 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        xDict = {}
+        yDict = {}
+        n = len(stones)
+        edges = []
+
+        for i, stone in enumerate(stones):
+            stoneX, stoneY = stone
+            if stoneX in xDict:
+                edges.append([i, xDict[stoneX]])
+            else:
+                xDict[stoneX] = i
+            
+            if stoneY in yDict:
+                edges.append([i, yDict[stoneY]])
+            else:
+                yDict[stoneY] = i
+        
+        parent = [i for i in range(n)]
+        ans = 0
+        for x, y in edges:
+            ans += self.union(parent, x, y)
+        return ans
+    
+    def union(self, parent, x, y):
+        root_x = self.find(parent, x)
+        root_y = self.find(parent, y)
+        if root_x != root_y:
+            parent[root_x] = root_y
+            return 1
+        return 0
+
+    def find(self, parent, x):
+        if parent[x] != x:
+            parent[x] = self.find(parent, parent[x])
+        return parent[x]
