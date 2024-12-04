d73816e0606a476aa5050a7b3fffff456bf63ae9
JennyDong1015 <zd66@cornell.edu>
2024-12-04 23:01:03 +0800

    review

--- medium/1905. Count Sub Islands.py
+++ medium/1905. Count Sub Islands.py
@@ -2,8 +2,10 @@ class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         m = len(grid2)
         n = len(grid2[0])
-        parent = {(i, j): (i, j) for i in range(m) for j in range(n) if grid2[i][j]}
-        direction = [(-1, 0), (0, -1)]
+        parent = {(i, j) : (i, j) for i in range(m) for j in range(n) if grid2[i][j]}
+        direction = [(0, -1), (-1, 0)]
+        islands = defaultdict(list)
+        cnt = 0
 
         for i in range(m):
             for j in range(n):
@@ -14,25 +16,20 @@ class Solution:
                         if 0 <= new_i < m and 0 <= new_j < n and grid2[new_i][new_j]:
                             self.union(parent, (i, j), (new_i, new_j))
 
-        islands = defaultdict(list)
-        for i in range(m):
-            for j in range(n):
-                if grid2[i][j]:
-                    root = self.find(parent, (i, j))
-                    islands[root].append((i, j))
-        
-        cnt = 0
+        for x in parent:
+            root = self.find(parent, x)
+            islands[root].append(x)
+
         for root, cells in islands.items():
-            isSubIsland = all(grid1[i][j] for (i, j) in cells)
-            if isSubIsland:
+            if all(grid1[i][j] for i, j in cells):
                 cnt += 1
         return cnt
-
+    
     def union(self, parent, x, y):
         root_x = self.find(parent, x)
         root_y = self.find(parent, y)
         if root_x != root_y:
-            parent[root_x] = root_y
+            parent[root_y] = root_x
     
     def find(self, parent, x):
         if x != parent[x]:
--- medium/959. Regions Cut By Slashes.py
+++ medium/959. Regions Cut By Slashes.py
@@ -1,41 +1,41 @@
 class Solution:
     def regionsBySlashes(self, grid: List[str]) -> int:
         n = len(grid)
-        total = 4 * n * n #top, right, bot, left
+        total = 4 * n * n
         parent = [i for i in range(total)]
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                idx = 4 * (i * n + j)
+        for i in range(n):
+            for j in range(n):
                 cell = grid[i][j]
+                idx = 4 * (i*n + j) #top(0), right(1), bot(2), left(3)
                 if cell == " ":
-                    self.union(parent, idx, idx + 1)
-                    self.union(parent, idx, idx + 2)
-                    self.union(parent, idx, idx + 3)
-                if cell == "/":
-                    self.union(parent, idx, idx + 3)
-                    self.union(parent, idx + 1, idx + 2)
-                if cell == "\\":
-                    self.union(parent, idx, idx + 1)
-                    self.union(parent, idx + 2, idx + 3)
+                    self.union(parent, idx, idx+1)
+                    self.union(parent, idx, idx+2)
+                    self.union(parent, idx, idx+3)
+                elif cell == "/":
+                    self.union(parent, idx, idx+3)
+                    self.union(parent, idx+1, idx+2)
+                elif cell == "\\":
+                    self.union(parent, idx, idx+1)
+                    self.union(parent, idx+2, idx+3)
                 if i < n-1:
-                    self.union(parent, idx+2, idx+(4*n))
+                    self.union(parent, idx+2, (idx+4*n))
                 if j < n-1:
-                    self.union(parent, idx+1, idx+4+3)
-        ans = 0
-        for i in range(total):
-            if i == self.find(parent, i):
-                ans += 1
-        return ans
+                    self.union(parent, idx+1, (idx+4+3))
 
+        regions = 0
+        for i in range(total):
+            root_i = self.find(parent, i)
+            if root_i == i:
+                regions += 1
+        return regions
+    
     def union(self, parent, x, y):
         root_x = self.find(parent, x)
         root_y = self.find(parent, y)
         if root_x != root_y:
-            parent[root_x] = root_y
-            return 1
-        return 0
-
+            parent[root_y] = root_x
+    
     def find(self, parent, x):
         if parent[x] != x:
             parent[x] = self.find(parent, parent[x])
