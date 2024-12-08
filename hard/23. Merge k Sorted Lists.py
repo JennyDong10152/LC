fc85765c403c47c5a277c42540051f9de796e842
JennyDong1015 <zd66@cornell.edu>
2024-12-08 22:54:10 +0800

    review

--- hard/23. Merge k Sorted Lists.py
+++ hard/23. Merge k Sorted Lists.py
@@ -5,18 +5,18 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not len(lists):
+        n = len(lists)
+        if not n:
             return None
-        if len(lists) == 1:
+        if n == 1:
             return lists[0]
-        mid = len(lists)//2
+        mid = n//2
         left = self.mergeKLists(lists[:mid])
         right = self.mergeKLists(lists[mid:])
         return self.merge(left, right)
     
     def merge(self, left, right):
         dummy = node = ListNode(0)
-
         while left is not None and right is not None:
             if left.val < right.val:
                 node.next = left
@@ -30,4 +30,4 @@ class Solution:
             node.next = left
         else:
             node.next = right
-        return dummy.next       
+        return dummy.next
