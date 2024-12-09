0fc0a54054ea909d80c55d66e6430fd75f950082
JennyDong1015 <zd66@cornell.edu>
2024-12-09 23:22:22 +0800

    Update LinkedList Template.py

--- LinkedList Template.py
+++ LinkedList Template.py
@@ -43,3 +43,34 @@ def findMidPoint(head):
             fast = fast.next.next
         mid = slow.next
         slow.next = None
+
+#merge two lists
+def mergeTwoLists(list1, list2):
+    if not list1:
+        return list2
+    if not list2:
+        return list1
+
+    if list1.val < list2.val:
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
+    else:
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
+
+def mergeTwoLists(list1, list2):
+    dummy = ListNode(0)
+    current = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next 
+
+    current.next = list1 if list1 else list2
+
+    return dummy.next
