85357b877a8ba9d2bc0702110ae1b27c71757f88
JennyDong1015 <zd66@cornell.edu>
2024-12-13 00:41:36 +0800

    review

--- medium/LinkedList/82. Remove Duplicates from Sorted List II/recursive.py
+++ medium/LinkedList/82. Remove Duplicates from Sorted List II/recursive.py
@@ -1,13 +1,9 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
         cur_val = head.val
+        
         if head.next.val == cur_val:
             while head and head.val == cur_val:
                 head = head.next
