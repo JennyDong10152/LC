91fab154c4c0150e61d1e09d9e8df40522fd86e4
JennyDong1015 <zd66@cornell.edu>
2025-01-03 23:01:38 +0800

    review

--- medium/Graph and Trees/Trees/255. Verify Preorder Sequence in Binary Search Tree.py
+++ medium/Graph and Trees/Trees/255. Verify Preorder Sequence in Binary Search Tree.py
@@ -1,14 +1,12 @@
 class Solution:
     def verifyPreorder(self, preorder: List[int]) -> bool:
-        min_limit = -float("inf")
+        minLimit = -float("inf")
         stack = []
-        
+
         for number in preorder:
             while stack and stack[-1] < number:
-                min_limit = stack.pop()
-                
-            if number <= min_limit:
+                minLimit = stack.pop()
+            if number <= minLimit:
                 return False
-            
             stack.append(number)
         return True
