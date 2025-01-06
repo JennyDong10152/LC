99e2f7f464d4d52234efeac654488731ce71e14c
JennyDong1015 <zd66@cornell.edu>
2025-01-06 20:57:58 +0800

    review

--- medium/Union Find/947. Most Stones Removed with Same Row or Column.py
+++ medium/Union Find/947. Most Stones Removed with Same Row or Column.py
@@ -3,33 +3,32 @@ class Solution:
         xDict = {}
         yDict = {}
         parent = [i for i in range(len(stones))]
-
         edges = []
-        ans = 0
+        answer = 0
 
         for i, [stoneX, stoneY] in enumerate(stones):
             if stoneX in xDict:
-                edges.append([xDict[stoneX], i])
+                edges.append((i, xDict[stoneX]))
             else:
                 xDict[stoneX] = i
             if stoneY in yDict:
-                edges.append([yDict[stoneY], i])
+                edges.append((i, yDict[stoneY]))
             else:
                 yDict[stoneY] = i
         
         for x, y in edges:
-            ans += self.union(parent, x, y)
-        return ans
+            answer += self.union(parent, x, y)
+        return answer
     
     def union(self, parent, x, y):
         root_x = self.find(parent, x)
         root_y = self.find(parent, y)
         if root_x != root_y:
-            parent[root_x] = root_y
+            parent[root_y] = root_x
             return 1
         return 0
     
     def find(self, parent, x):
-        if parent[x] != x:
+        if x != parent[x]:
             parent[x] = self.find(parent, parent[x])
         return parent[x]
