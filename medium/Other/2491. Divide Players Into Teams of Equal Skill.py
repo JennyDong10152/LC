9f6a9af355d0a07babb23e1f9774913f287c6d45
JennyDong1015 <zd66@cornell.edu>
2025-01-10 21:28:00 +0800

    review

--- medium/Other/2491. Divide Players Into Teams of Equal Skill.py
+++ medium/Other/2491. Divide Players Into Teams of Equal Skill.py
@@ -3,10 +3,9 @@ class Solution:
         skill.sort()
         n = len(skill)
         chemistry = 0
-
-        target = skill[0] + skill[-1]
-        for i in range(n//2):
-            if skill[i] + skill[n-i-1] != target:
+        target = skill[0] + skill[n-1]
+        for idx in range(n//2):
+            if skill[idx] + skill[n-1-idx] != target:
                 return -1
-            chemistry += skill[i] * skill[n-i-1]
+            chemistry += (skill[idx] * skill[n-1-idx])
         return chemistry
