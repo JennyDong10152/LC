aaf9c2edd77a6e00ac754f4880fceff0d579c88d
JennyDong1015 <zd66@cornell.edu>
2025-01-11 23:01:33 +0800

    review

--- medium/Graph/841. Keys and Rooms.py
+++ medium/Graph/841. Keys and Rooms.py
@@ -1,14 +1,14 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        total = len(rooms)
-        visited = set()
+        n = len(rooms)
         queue = deque([0])
-
+        visited = set()
+        
         while queue:
-            current = queue.popleft()
-            visited.add(current)
-            for neighbor in rooms[current]:
-                if neighbor in visited:
-                    continue
-                queue.append(neighbor)
-        return total == len(visited)
+            room = queue.popleft()
+            if room in visited:
+                continue
+            visited.add(room)
+            for nextRoom in rooms[room]:
+                queue.append(nextRoom)
+        return len(visited) == n
