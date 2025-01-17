6d3b1ba7764fba9cf32a82f414bfca4729c0492f
JennyDong1015 <zd66@cornell.edu>
2025-01-17 21:08:59 +0800

    review

--- medium/Backtrack/46. Permutations.py
+++ medium/Backtrack/46. Permutations.py
@@ -1,16 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         self.answer = []
-        self.visited = set()
         self.nums = sorted(nums)
+        self.visited = set()
         self.search([])
         return self.answer
     
     def search(self, temp):
         if len(temp) == len(self.nums):
             self.answer.append(temp)
-            return
-        
+            return 
+            
         for idx in range(len(self.nums)):
             if idx in self.visited:
                 continue
