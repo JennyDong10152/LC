b06c1eaca10d26c980555b77b2ac313ab1300280
Jenny Dong <jennydong2673@gmail.com>
2025-09-26 21:48:56 -0700

    new

--- medium/Greedy/921. Minimum Add to Make Parentheses Valid.py
+++ medium/Greedy/921. Minimum Add to Make Parentheses Valid.py
@@ -1,12 +1,11 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         stack = []
-
         for char in s:
             if char == '(':
-                stack.append(char)
+                stack.append(char) 
             else:
-                if stack and stack[-1] == '(':
+                if stack and stack[-1] == "(":
                     stack.pop()
                 else:
                     stack.append(char)
