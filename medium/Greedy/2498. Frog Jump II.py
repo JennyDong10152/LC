d7ede4afa0b04bbb643729718f4a88a15ac31f97
Jenny Dong <jennydong2673@gmail.com>
2026-09-22 22:24:02 -0700

    new

--- medium/Greedy/2498. Frog Jump II.py
+++ medium/Greedy/2498. Frog Jump II.py
@@ -1,7 +1,8 @@
 class Solution:
-    def maxJump(self, stones: List[int]) -> int:
+    def maxJump(self, stones: list[int]) -> int:
         n = len(stones)
         answer = stones[1]
+
         for idx in range(n-2):
-            answer = max(answer, stones[idx+2]-stones[idx])
+            answer = max(answer, stones[idx+2] - stones[idx])
         return answer
