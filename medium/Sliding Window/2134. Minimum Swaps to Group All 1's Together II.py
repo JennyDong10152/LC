a10acfa9bee918bd863fe5175611995187f6ff34
Jenny Dong <jennydong2673@gmail.com>
2026-09-26 00:04:08 -0700

    review 9/25

--- medium/Sliding Window/2134. Minimum Swaps to Group All 1's Together II.py
+++ medium/Sliding Window/2134. Minimum Swaps to Group All 1's Together II.py
@@ -2,9 +2,10 @@ class Solution:
     def minSwaps(self, nums: list[int]) -> int:
         window = sum(nums)
         swap = minSwap = nums[:window].count(0)
-        
-        for idx in range(window, window + len(nums)):
-            swap -= not nums[(idx - window) % len(nums)]
-            swap += not nums[idx % len(nums)]
+        n = len(nums)
+
+        for idx in range(window, window+n):
+            swap -= not nums[(idx-window) % n]
+            swap += not nums[idx % n]
             minSwap = min(minSwap, swap)
         return minSwap
